5625a001a8099cda65d6894e78d62704b184fee3
Blake <blakepetersont@gmail.com>
2022-11-16 15:27:10 -0700

    added to the if than statements to include a lowercase y as an accepted input

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -24,24 +24,24 @@ function generatePassword() {
   var useableCharacters = [];
   // created an if then statement and prompt to determine if there would be capital and lowercase letters in the password
   userStatement = prompt("Include Uppercase Letters? Y/(N)");
-  if (userStatement === "Y") {
+  if (userStatement === "Y", "y") {
    useableCharacters.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 
   }
   userStatement = prompt("Include Lowercase Letters? Y/(N)");
-  if (userStatement === "Y") {
+  if (userStatement === "Y", "y") {
    useableCharacters.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
    
   }
   // created an if then statement and prompt to determine if numbers would be included in the password
   userStatement = prompt("Include Numbers? Y/(N)");
-  if (userStatement === "Y") {
+  if (userStatement === "Y", "y") {
    useableCharacters.push("0","1","2","3","4","5","6","7","8","9");
    
   }
   // created an if then statement and prompt to determine if specail characters would be included in the password
   userStatement = prompt("Include Special Characters? Y/(N)");
-  if (userStatement === "Y") {
+  if (userStatement === "Y", "y") {
    useableCharacters.push(" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".","/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "\"");
    
   }
